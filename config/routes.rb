afa736f47755d0104bde625d8c74ef21b716578b
ruangtiwa <st120534@ait.asia>
2019-02-06 20:47:12 +0700

    edited the diary model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
 devise_for :users
-resources :diaries  
+resources :users do
+	resources :diaries
+end
+  
 root to: "diaries#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
