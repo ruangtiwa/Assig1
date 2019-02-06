71c59b161846d1be1bc798177f7ffb00a7e34ce7
Hom <st120225@ait.asia>
2019-02-06 18:13:06 +0700

    Create User!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :diaries
+  devise_for :users
+  resources :diaries 
+  root to: "diaries#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
