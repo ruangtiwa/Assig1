4b981076820c528d80f3295162066d9034e1c8e7
“Phanthawit” <mahibaz.cagoas@gmail.com>
2019-02-07 03:20:02 -0800

    change in sign_in page

--- db/migrate/20190206130705_add_user_to_diaries.rb
+++ db/migrate/20190206130705_add_user_to_diaries.rb
@@ -1,5 +1,5 @@
-=begin class AddUserToDiaries < ActiveRecord::Migration[5.2]
+class AddUserToDiaries < ActiveRecord::Migration[5.2]
   def change
     add_reference :diaries, :user, foreign_key: true
   end
-end =end
+end 
